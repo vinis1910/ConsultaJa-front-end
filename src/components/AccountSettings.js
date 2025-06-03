e631bde6d86d9be32a626b3fdb678bb4a0b8cc5b
Rafael Roseira Machado <rafaroseira27@outlook.com>
2025-06-02 21:07:34 -0300

    remove required from password

--- src/components/AccountSettings.js
+++ src/components/AccountSettings.js
@@ -109,7 +109,7 @@ const AccountSettings = () => {
               name="password"
               value={formData.password}
               onChange={handleChange}
-              required
+              
             />
           </div>
 
